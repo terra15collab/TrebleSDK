9342eb0b1ec520e1130609c07a122901d2110fe0
Simon Stobie <simon.stobie@terra15.com.au>
2023-03-29 10:46:57 +0800

    Added plot_traces.py example.

---
+++ HDF5_samples/plot_traces.py
@@ -0,0 +1,147 @@
+"""Demo script to load strainrate data and visualise individual traces."""
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+
+### Set parameters ###############################################################
+fname = r"../sample_data/example_triggered_shot.hdf5"
+T_DURATION = 2 # (s)
+##########################################################################################
+
+def convert_velocity_to_strainrate(data, gauge_length_m, dx):
+    gauge_samples = int(round(gauge_length_m / dx))
+    return (data[:, gauge_samples:] - data[:, :-gauge_samples]) / (gauge_samples * dx)
+
+def correct_gauge_length_offset(x_vector, gauge_length):
+    """Compensate for distance shift of data caused by gauge_length calculation."""
+    # crops end of x_vector by gauge length
+    dx = x_vector[1] - x_vector[0]
+    gauge_samples = int(round(gauge_length / dx))
+    gauge_length = gauge_samples * dx
+    x_correct = x_vector[:-gauge_samples]
+
+    # compensates for GL/2 signal offset
+    x_correct = x_correct + gauge_length / 2
+    return x_correct
+
+
+def load_hdf_slice(filepath, t_start=None, t_duration=None, x_start=None, x_stop=None, info=True):
+    """
+        Loads data and metadata from .hdf5 file. Optionally slices data in time and space.
+
+        Args:
+            file_path: hdf5 file path.
+            t_start: Time from start of data to begin slice (s)
+            t_duration: Length of desired data slice (s)
+            x_start: Start fibre distance from front of Treble (m)
+            x_stop: Stop fibre distance from front of Treble (m)
+
+        Returns:
+            data: Sliced data array.
+            metadata: Attributes of hdf file.
+            timestamps: Sliced timestamp vector.
+            distances: Sliced distance vector.
+
+        """
+    print(f"Loading: {filepath}")
+
+    with h5py.File(filepath, "r") as f:
+        md = dict(f.attrs)
+        dt = md["dt_computer"]
+        dx = md["dx"]
+
+        # Accesses full data but does not load into memory
+        data = f["data_product"]["data"]
+        if "gps_time" in f["data_product"].keys():
+            t = f["data_product"]["gps_time"]
+        else:
+            print("Using Computer Time, either because no GPS data was recorded or this is a re-saved dataset.")
+            t = f["data_product"]["posix_time"]
+        x = np.arange(md["nx"]) * md["dx"] + md["sensing_range_start"]
+
+        if info is True:
+            # Prints dimensions of full data
+            print(f"Full Dataset Properties:")
+            print(f"    Data Shape:         {data.shape}")
+            print(f"    t_end - t_start:    {t[-1]-t[0]:.8f} s")
+            print(f"    nt * dt_computer:   {t.shape[0] * dt}")
+            print(f"    Timestamps:         [{t[0]:.2f} : {t[-1]:.2f}] ({t[-1] - t[0]:.2f}s)")
+            print(f"    Times (UTC):        [{datetime.utcfromtimestamp(t[0])} : {datetime.utcfromtimestamp(t[-1])}]")
+            print(f"    Distance:           [{x[0]:.1f} : {x[-1]:.1f}] m")
+
+        # Calculates slice boundaries in samples
+        if t_start:
+            t1 = int(t_start / dt)
+        else:
+            t1 = 0
+        if t_duration:
+            t2 = t1 + int(t_duration / dt)
+            # Enforces limit on end of data.
+            if t2 > len(t):
+                t2 = len(t)
+        else:
+            t2 = None
+
+        if x_start:
+            x1 = int((x_start-md["sensing_range_start"])/dx)
+        else:
+            x1 = None
+        if x_stop:
+            x2 = int((x_stop-md["sensing_range_start"])/dx)
+        else:
+            x2 = None
+
+        # Slices data in space and time.
+        # Only the SLICE of data is loaded into memory
+        data = data[t1:t2, x1:x2]
+        t = t[t1:t2]
+        x = x[x1:x2]
+
+        if info is True:
+            # Prints dimensions of sliced output data
+            print(f"Loading data slice:")
+            print(f"    Data Shape:         {data.shape}")
+            print(f"    t_end - t_start:    {t[-1]-t[0]:.8f} s")
+            print(f"    nt * dt_computer:   {t.shape[0] * dt}")
+            print(f"    Timestamps:         [{t[0]:.2f} : {t[-1]:.2f}] ({t[-1] - t[0]:.2f}s)")
+            print(f"    Times (UTC):        [{datetime.utcfromtimestamp(t[0])} : {datetime.utcfromtimestamp(t[-1])}]")
+            print(f"    Distance:           [{x[0]:.1f} : {x[-1]:.1f}] m")
+        return data, md, t, x
+
+
+def trace_plot(data, x_vector, t_vector, ax=None):
+    # Normalise data to dimensionless y-values which align to x-samples.
+    dx = x_vector[1] - x_vector[0]
+    norm_factor = np.std(data) * 4
+    norm_data = (data / norm_factor) * dx
+    norm_data = norm_data + x_vector
+
+    if ax:
+        plt.sca(ax)
+
+    plt.plot(t_vector, norm_data, linewidth=0.5, c="b")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Fibre Distance (m)")
+    plt.xlim((t_vector[0], t_vector[-1]))
+    plt.ylim((np.min(norm_data), np.max(norm_data)))
+    plt.gca().invert_yaxis()
+
+data, md, t, x = load_hdf_slice(fname, t_duration=T_DURATION)
+
+if md["data_product"] == "velocity":
+    data = convert_velocity_to_strainrate(data, md["pulse_length"], md["dx"])
+    x = correct_gauge_length_offset(x, md["pulse_length"])
+
+plt.figure(figsize=(12,7))
+trace_plot(
+    data,
+    x,
+    t-t[0],
+    ax=plt.gca()
+)
+plt.title(f"Strainrate Traces\n{fname}")
+plt.tight_layout()
+plt.savefig("plot_traces.png")
+plt.show()
