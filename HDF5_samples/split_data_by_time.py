6bf09546039769d0a5ff9c5d94a06f7b8b8b9549
Simon Stobie <simon.stobie@terra15.com.au>
2023-08-01 09:46:14 +0800

    split_data_by_time.py copies all original file attributes.

--- HDF5_samples/split_data_by_time.py
+++ HDF5_samples/split_data_by_time.py
@@ -7,9 +7,9 @@ Virtual data files represent links to the original dataset, and must remain in t
 #####################################################################################################################
 
 # Define the path to the .h5 virtual hdf5 file or single .hdf5 file to split.
-hdf5_data_file = "../sample_data/v5_compatible.h5"
+# hdf5_data_file = "../sample_data/v5_compatible.h5"
 # OR
-# hdf5_data_file = "../sample_data/example_triggered_shot.hdf5"
+hdf5_data_file = "../sample_data/example_triggered_shot.hdf5"
 
 # DESIRED OUTPUT FILE DURATION
 file_duration = 0.5 # seconds
@@ -22,84 +22,117 @@ import h5py
 from datetime import datetime
 import numpy as np
 
+
 def correct_timestamp_format(timestamp):
     return datetime.utcfromtimestamp(timestamp).strftime('UTC-YMD%Y%m%d-HMS%H%M%S.%fZ')
+
 
 def split_hdf5_file(source_hdf5_file, split_duration):
     save_directory = hdf5_data_file.parent.joinpath(f"{split_duration:.1f}s_split_files")
     save_directory.mkdir(exist_ok=True)
 
-    # Get original file parameters
+    # Get split parameters from source file
     with h5py.File(source_hdf5_file, "r") as f_in:
-        print("Splitting Datasets from Original File:")
-        print(source_hdf5_file)
-
-        metadata = dict(f_in.attrs)
-        group = f_in["data_product"]
-        dataset_names = list(group.keys())
-
-        dataset_info = {}
-        for dset_name in dataset_names:
-            print(group[dset_name])
-            dataset_info[dset_name] = dict(group[dset_name].attrs)
-
         # determines number of samples in each split file
-        n_split = int(split_duration/metadata['dt_computer'])
-        nt = group['data'].shape[0]
-        nx = group['data'].shape[1]
+        n_split = int(split_duration/f_in.attrs.get("dt_computer"))
+        nt = f_in["data_product/data"].shape[0]
+        nx = f_in["data_product/data"].shape[1]
 
         # determines split indexes and times from original dataset
-        split_indexes = np.arange(0, nt, n_split)
-        if "gps_time" in dataset_names:
-            split_times = group["gps_time"][split_indexes]
-        else:
-            split_times = group["posix_time"][split_indexes]
+        split_indices = np.arange(0, nt, n_split)
+        try:
+            split_times = f_in["data_product/gps_time"][split_indices]
+        except KeyError:
+            split_times = f_in["data_product/posix_time"][split_indices]
 
-    # Creates virtual mapping to new .vhdf5 files
-    split_filenames = []
-    for n, (i1, t1) in enumerate(zip(split_indexes[:], split_times[:])):
+    # Creates mapping to new .vhdf5 files
+    for n, (i1, t1) in enumerate(zip(split_indices[:], split_times[:])):
         dest_fname = save_directory.joinpath(f"{correct_timestamp_format(t1)}_seq_{str(n).zfill(11)}.vhdf5")
-        split_filenames.append(dest_fname)
         print(dest_fname)
 
-        with h5py.File(dest_fname, "w") as f_out:
-            # checks for last file case.
-            if i1 + n_split > nt:
-                n_split = nt - i1
-            # creates virtual dataset for "data" dataset
-            layout = h5py.VirtualLayout(shape=(n_split, nx), dtype=np.float32)
-            vsource = h5py.VirtualSource(source_hdf5_file, "data_product/data", shape=(nt, nx), dtype=np.float32)
-            layout[:] = vsource[i1:i1 + n_split]
-            f_out.create_virtual_dataset("data_product/data", layout, fillvalue=0)
-            print(f_out['data_product/data'])
+        # checks for last file case.
+        if i1 + n_split > nt:
+            n_split = nt - i1
 
-            # creates virtual datasets for time arrays
-            for dset_name in dataset_names[1:]:
-                layout = h5py.VirtualLayout(shape=(n_split,), dtype=np.float64)
-                vsource = h5py.VirtualSource(source_hdf5_file, f"data_product/{dset_name}", shape=(nt, ), dtype=np.float64)
-                layout[:] = vsource[i1:i1 + n_split]
-                f_out.create_virtual_dataset(f"data_product/{dset_name}", layout, fillvalue=0)
-                print(f_out[f"data_product/{dset_name}"])
+        # splits main dataset
+        split_dataset_virtual(
+            "data_product/data",
+            source_hdf5_file,
+            dest_fname,
+            (n_split, nx),
+            i1,
+        )
+        # splits time arrays
+        for dset_name in ["posix_time", "gps_time"]:
+            try:
+                split_dataset_virtual(
+                    f"data_product/{dset_name}",
+                    source_hdf5_file,
+                    dest_fname,
+                    (n_split,),
+                    i1
+                )
+            except KeyError:
+                print(f"Dataset {dset_name} not found in original file.")
 
-            # change key top-level attributes
-            metadata["file_start_computer_time"] = f_out["data_product/posix_time"][0]
-            metadata["file_start_computer_time_string"] = correct_timestamp_format(f_out["data_product/posix_time"][0])
-            if "gps_time" in dataset_names:
-                metadata["file_start_gps_time"] = f_out["data_product/gps_time"][0]
-                print(f"File Duration: {f_out['data_product/gps_time'][-1] - f_out['data_product/gps_time'][0]}s")
-            else:
-                metadata["file_start_gps_time"] = "FALSE"
-                print(f"File Duration: {f_out['data_product/posix_time'][-1] - f_out['data_product/posix_time'][0]}s")
+        # copies diagnostic data
+        with h5py.File(source_hdf5_file, "r") as f_in:
+            with h5py.File(dest_fname, "a") as f_out:
+                f_in.copy("diagnostics", f_out)
 
-            metadata["nt"]=n_split
-            f_out.attrs.update(metadata)
+        # copies attributes from original file
+        copy_dataset_attributes(source_hdf5_file, dest_fname, ".")
+        copy_dataset_attributes(source_hdf5_file, dest_fname, "data_product")
+        fix_timing_attributes(dest_fname)
 
-            # Copies attributes for each dataset
-            for dset_name, attrs in dataset_info.items():
-                f_out[f"data_product/{dset_name}"].attrs.update(attrs)
+        with h5py.File(dest_fname, "r") as f_out:
+            print(f_out["data_product/data"])
+            try:
+                print(f"File Length: {f_out['data_product/gps_time'][-1] - f_out['data_product/gps_time'][0]}s")
+            except KeyError:
+                print(f"File Length: {f_out['data_product/posix_time'][-1] - f_out['data_product/posix_time'][0]}s")
 
-    return split_filenames
+    return save_directory, list(save_directory.glob("*.vhdf5"))
 
 
-hdf5_data_file = Path(hdf5_data_file)
-split_filenames = split_hdf5_file(hdf5_data_file, file_duration)
+def split_dataset_virtual(dataset_name, source_fname, dest_fname, dest_shape, i1):
+    # gets source file parameters
+    with h5py.File(source_fname, "r") as f_in:
+        try:
+            source_shape = f_in[dataset_name].shape
+            dtype = f_in[dataset_name].dtype
+        except KeyError:
+            print(f"Dataset {dataset_name} not found in original file.")
+
+    # Maps source file to virtual dataset
+    with h5py.File(dest_fname, "a") as f_out:
+        vsource = h5py.VirtualSource(source_fname, dataset_name, shape=source_shape, dtype=dtype)
+        layout = h5py.VirtualLayout(shape=dest_shape, dtype=dtype)
+        layout[:] = vsource[i1:i1 + dest_shape[0]]
+        f_out.create_virtual_dataset(dataset_name, layout, fillvalue=0)
+
+    copy_dataset_attributes(source_fname, dest_fname, dataset_name)
+
+
+def copy_dataset_attributes(source_file, dest_file, dataset_name):
+    with h5py.File(source_file, "r") as f_in:
+        with h5py.File(dest_file, "a") as f_out:
+            attrs = dict(f_in[dataset_name].attrs)
+            f_out[dataset_name].attrs.update(attrs)
+
+
+def fix_timing_attributes(dest_file):
+    with h5py.File(dest_file, "a") as f_out:
+        correct_attributes = {
+            "file_start_computer_time": f_out["data_product/posix_time"][0],
+            "file_start_computer_time_string": correct_timestamp_format(f_out["data_product/posix_time"][0]),
+            "nt": f_out["data_product/data"].shape[0],
+        }
+        try: correct_attributes.update({"file_start_gps_time": f_out["data_product/gps_time"][0]})
+        except KeyError: correct_attributes.update({"file_start_gps_time":f_out["data_product/posix_time"][0]})
+
+        f_out.attrs.update(correct_attributes)
+
+
+hdf5_data_file = Path(hdf5_data_file).resolve()
+save_directory, split_files = split_hdf5_file(hdf5_data_file, file_duration)
